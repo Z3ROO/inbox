00e95a8aecea9b1da0f07a03db6624b5a10ca57c
zero <zero@sara.com>
2023-01-21 09:06:13 -0300

    refactor: inbox panel controlls and inputField

--- src/features/inbox/index.tsx
+++ src/features/inbox/index.tsx
@@ -13,39 +13,45 @@ export default function Inbox() {
 }
 
 function InboxPanel() {
-  const { insertInbox, toggleFilterInboxMode } = useInboxState()!;
-
   return (
     <div>
       <h4 className="text-tanj-green">Inbox</h4>
       <div className="">
-        <AddItemsField />
-        <div className="flex justify-between">
-          <BtnPrimary
-            onClick={insertInbox}
-          >Add</BtnPrimary>
-          <BtnPrimary
-            onClick={toggleFilterInboxMode}
-          >Free</BtnPrimary>
-        </div>
+        <InboxPanelInputField />
+        <InboxPanelControlls />
       </div>
       <FilterInboxModal />      
     </div>
   )
 }
 
-function AddItemsField() {
+function InboxPanelInputField() {
   const {inboxInsertText, setInboxInsertText} = useInboxState()!;
 
   return (
     <textarea 
-    value={inboxInsertText} onChange={e => setInboxInsertText(e.target.value)}
-    className={`
-      resize-none bg-tanj-white rounded-sm w-72 h-36 p-2 
-      shadow-inner shadow-[rgba(0,0,0,0.2)] 
-      border-2 border-tanj-pink focus:border-tanj-green outline-none
-    `} 
+      value={inboxInsertText} onChange={e => setInboxInsertText(e.target.value)}
+      className={`
+        resize-none bg-tanj-white rounded-sm w-72 h-36 p-2 
+        shadow-inner shadow-[rgba(0,0,0,0.2)] 
+        border-2 border-tanj-pink focus:border-tanj-green outline-none
+      `} 
     />
+  )
+}
+
+function InboxPanelControlls() {
+  const { insertInbox, toggleFilterInboxMode } = useInboxState()!;
+
+  return (
+    <div className="flex justify-between">
+      <BtnPrimary
+        onClick={insertInbox}
+      >Add</BtnPrimary>
+      <BtnPrimary
+        onClick={toggleFilterInboxMode}
+      >Free</BtnPrimary>
+    </div>
   )
 }
 

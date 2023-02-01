51a33b0338ea45cc2440006c04ad49ebcb836a5c
zero <theloostt@live.com>
2023-02-01 12:12:14 -0300

    feat: new Coding icon

--- frontend/src/components/icons/kagura.tsx
+++ frontend/src/components/icons/kagura.tsx
@@ -97,6 +97,14 @@ export function JS_DSA(props: IconProps) {
   )
 }
 
+export function Coding(props: IconProps) {
+  return (
+    <svg {...props} viewBox="0 0 16 16">
+      <path d="M5.15,5,3.88,3.76.29,7.34a.91.91,0,0,0,0,1.28l3.64,3.64L5.2,11l-3-3Zm10.6,2.31L12.16,3.76,10.89,5l2.95,3-3,3,1.27,1.28,3.64-3.64A.91.91,0,0,0,15.75,7.34ZM5.63,13.78l1.74.44,3-12L8.61,1.78Z"/>
+    </svg>
+  )
+}
+
 
 /*
 

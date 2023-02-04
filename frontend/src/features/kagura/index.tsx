f1d100d132dd6919333bec9a50dcc4ddee85fd75
zero <theloostt@live.com>
2023-02-04 14:36:56 -0300

    feat: implement config options component for card routine

--- frontend/src/features/kagura/index.tsx
+++ frontend/src/features/kagura/index.tsx
@@ -1,5 +1,8 @@
 import { HiPlus } from 'react-icons/hi';
-import { FaReact } from 'react-icons/fa';
+import { FaReact, FaCheck } from 'react-icons/fa';
+import { GoGear } from 'react-icons/go';
+import { ImCross } from 'react-icons/im';
+import { FaTrashAlt } from "react-icons/fa";
 import { GoThumbsup, GoThumbsdown, GoPrimitiveDot } from 'react-icons/go';
 import { Modal } from "@/components/Modal";
 import { useEffect, useMemo, useRef, useState } from "react";
@@ -265,7 +268,8 @@ function RoutineBody() {
   },[requirements]);
 
   return (
-    <div className=''>
+    <div className='relative'>
+      <RoutineOptions />
       <div className='w-80 h-64 p-4 m-2 overflow-auto custom-scrollbar'>
         <span className='text-tanj-green select-none'>
           {requirements}
@@ -297,6 +301,86 @@ function RoutineBody() {
           <GoThumbsup className='p-1 w-7 h-7' />   
         </BtnSecondary>
       </div>
+    </div>
+  )
+}
+
+function RoutineOptions() {
+  const [isOpen, setIsOpen] = useState(false);
+  const divStyle: React.CSSProperties = {
+    backgroundColor: 'rgba(70, 176, 119, .1)',
+    backdropFilter: 'blur(8px)'
+  };
+
+  return (
+    <div style={isOpen ? divStyle : {} } className='absolute -top-4 right-0 rounded-sm'>
+      <BtnSecondary icon 
+        className=''
+        onClick={e => setIsOpen(prev => !prev)} 
+      >
+        <GoGear />
+      </BtnSecondary>
+
+      {
+        isOpen &&
+        <div className='flex flex-col'>
+          <RoutineOptionsItem confirm 
+            action={() => {
+              setIsOpen(false);
+            }}
+          >
+            <FaTrashAlt />
+          </RoutineOptionsItem>          
+        </div>
+      }
+    </div>
+  )
+}
+
+function RoutineOptionsItem(props: { action: () => void, confirm?: boolean, children: JSX.Element}) {
+  const [isConfirmOpen, setIsConfirmOpen] = useState(false);
+  const { action, confirm, children } = props;
+
+  return (
+    <div className='relative'>
+      <BtnSecondary icon 
+        className=''
+        onClick={e => {
+          if (confirm) {
+            setIsConfirmOpen(prev => !prev);
+            return;
+          }
+          action();
+        }}
+      >
+        {children}
+      </BtnSecondary>
+      { 
+        isConfirmOpen && ( 
+          <ConfirmationWidget 
+            className='absolute top-0.5 left-[calc(100%+.5rem)]'
+            y={() => { action(); setIsConfirmOpen(false); }} 
+            n={() => setIsConfirmOpen(false)} 
+          /> 
+        )
+      }
+    </div>
+  )
+}
+
+function ConfirmationWidget({y, n, className}: { y: () => void, n: () => void, className?: string}) {
+  return (
+    <div 
+      style={{
+        backdropFilter: 'blur(8px)'
+      }}
+      className={`flex rounded-sm bg-tanj-green bg-opacity-10 ${className}`}>
+      <BtnSecondary icon onClick={n}>
+        <ImCross className='w-2.5 h-2.5' />
+      </BtnSecondary>
+      <BtnSecondary icon onClick={y}>
+        <FaCheck className='w-2.5 h-2.5' />
+      </BtnSecondary>
     </div>
   )
 }

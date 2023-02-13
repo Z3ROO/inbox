c6562712c9be1119ac89deb57be3953fdbd4aa31
zero <theloostt@live.com>
2023-02-12 22:48:28 -0300

    refactor: wrap submit button

--- frontend/src/features/kagura/components/AddCard.tsx
+++ frontend/src/features/kagura/components/AddCard.tsx
@@ -32,7 +32,13 @@ function OpenModalButton({ setIsModalOpen }: { setIsModalOpen: React.Dispatch<Re
 }
 
 function AddCardForm({ setIsModalOpen }: { setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>> }) {
-  const { insertCard } = useKagura()!;
+  const [typesOptions, setTypesOptions] = useState<{label: string, value: string}[]>([]);
+  const [categoriesOptions, setCategoriesOptions] = useState<{label: string, value: string}[]>([]);
+
+  const [difficulty, setDifficulty] = useState<1|2|3>(3);
+  const [typeInput, setTypeInput] = useState(['', '']);
+  const [requirementsInput, setRequirementsInput] = useState('');
+  const [categoryInput, setCategoryInput] = useState(['', '']);
 
   useEffect(() => {
     (async () => {
@@ -48,14 +54,6 @@ function AddCardForm({ setIsModalOpen }: { setIsModalOpen: React.Dispatch<React.
       ));
     })();
   },[]);
-
-  const [typesOptions, setTypesOptions] = useState<{label: string, value: string}[]>([]);
-  const [categoriesOptions, setCategoriesOptions] = useState<{label: string, value: string}[]>([]);
-
-  const [difficulty, setDifficulty] = useState<1|2|3>(3);
-  const [typeInput, setTypeInput] = useState(['', '']);
-  const [requirementsInput, setRequirementsInput] = useState('');
-  const [categoryInput, setCategoryInput] = useState(['', '']);
 
   return (
     <div className='p-2'>
@@ -75,30 +73,10 @@ function AddCardForm({ setIsModalOpen }: { setIsModalOpen: React.Dispatch<React.
           <Textarea className={`w-full resize-none h-32`} value={requirementsInput} onChange={e => setRequirementsInput(e.target.value)} />
         </label>
       </form>
-      <BtnPrimary type="submit" 
-        form='insert-card-form'
-        onClick={e => {
-          e.preventDefault();
-          
-          insertCard.mutate(
-            { 
-              type: typeInput[1] !== '' ? typeInput[1] : typeInput[0], 
-              requirements: requirementsInput, 
-              category: categoryInput[1] !== '' ? categoryInput[1] : categoryInput[0],
-              difficulty
-            },
-            {
-              onSuccess: () => {
-                setIsModalOpen(false);
-              }
-            }
-          );
-        }}
-      >Create</BtnPrimary>
+      <Submit {...{typeInput, categoryInput, requirementsInput, difficulty, setIsModalOpen }} />
     </div>
   )
 }
-
 
 function DifficultyField({ difficulty, setDifficulty }: {difficulty: ( 1 | 2 | 3 ), setDifficulty: React.Dispatch<React.SetStateAction<2 | 1 | 3>>}) {
   return (
@@ -130,5 +108,33 @@ function DifficultyField({ difficulty, setDifficulty }: {difficulty: ( 1 | 2 | 3
         </div>           
       </div>
     </div>
+  )
+}
+
+function Submit(props: {typeInput: string[], categoryInput: string[], requirementsInput: string, difficulty: ( 1 | 2 | 3 ) }, setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>) {
+  const { insertCard } = useKagura()!;
+  const { typeInput, categoryInput, requirementsInput, difficulty } = props;
+
+  return (
+    <BtnPrimary type="submit" 
+      form='insert-card-form'
+      onClick={e => {
+        e.preventDefault();
+        
+        insertCard.mutate(
+          { 
+            type: typeInput[1] !== '' ? typeInput[1] : typeInput[0], 
+            requirements: requirementsInput, 
+            category: categoryInput[1] !== '' ? categoryInput[1] : categoryInput[0],
+            difficulty
+          },
+          {
+            onSuccess: () => {
+              setIsModalOpen(false);
+            }
+          }
+        );
+      }}
+    >Create</BtnPrimary>
   )
 }

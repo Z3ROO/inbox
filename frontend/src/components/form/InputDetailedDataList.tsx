3b62f0990167ee653dea8d50fb92d9645b446f28
zero <theloostt@live.com>
2023-02-22 23:57:33 -0300

    feat: implement final touchs

--- frontend/src/components/form/InputDetailedDataList.tsx
+++ frontend/src/components/form/InputDetailedDataList.tsx
@@ -1,5 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { GiCaravel } from 'react-icons/gi';
+import { HiExclamationCircle } from "react-icons/hi2";
 import { BtnPrimary } from "../Buttons";
 import { Input } from "./Input";
 
@@ -14,15 +15,17 @@ export interface DetailedDataList {
   options: OptionType[];
   className?: string;
   onSubmit?: () => void
-  onSelect?: () => void
+  onSelect?: (option: OptionType) => void
 }
 
 interface InputDetailedDataListContext {
+  value: OptionType|undefined;
   onSubmit: (() => void) | undefined;
   inputText: string;
   setInputText: React.Dispatch<React.SetStateAction<string>>;
   filteredOptions: OptionType[];
-  selectOption: (option: OptionType) => void
+  autoSelectOptionIfMatch: (textContent: string) => void;
+  selectOption: (option: OptionType) => void;
 }
 
 const Context = createContext<InputDetailedDataListContext|null>(null);
@@ -36,7 +39,7 @@ export function InputDetailedDataList(props: DetailedDataList) {
   function autoSelectOptionIfMatch(textContent: string) {
     const option = options.find(opt => textContent === opt.label);
     if (option) {
-      setValue({label: option.value, value: option.value});
+      setValue({label: option.label, value: option.value});
     }
     else {
       setValue({label: textContent, value: ''});
@@ -44,9 +47,10 @@ export function InputDetailedDataList(props: DetailedDataList) {
   }
 
   function selectOption(option: OptionType) {
+    setInputText(option.label);
     setValue(option);
     if (onSelect)
-      onSelect();
+      onSelect(option);
   }
 
   useEffect(() => {
@@ -56,6 +60,7 @@ export function InputDetailedDataList(props: DetailedDataList) {
   }, [inputText, options]);
 
   const contextValue = {
+    value,
     onSubmit,
     inputText, setInputText,
     filteredOptions, 
@@ -73,24 +78,38 @@ export function InputDetailedDataList(props: DetailedDataList) {
 }
 
 function InputContainer() {
-  const { inputText, setInputText, onSubmit } = useContext(Context)!;
+  const { inputText, setInputText, onSubmit, autoSelectOptionIfMatch } = useContext(Context)!;
   return (
     <>
       <div className="flex">
-        <Input 
-          className="mx-2 grow" 
-          value={inputText} 
-          onChange={(e) => { 
-            setInputText(e.target.value);
-
-          }} />
+        <div className="mx-2 w-full relative">
+          <Input 
+            className="w-full" 
+            value={inputText} 
+            onChange={(e) => { 
+              setInputText(e.target.value);
+              autoSelectOptionIfMatch(e.target.value);
+            }} />
+          <InexistentOptionIconWarning />
+        </div>
         {
-          onSubmit && <BtnPrimary className="m-0 mx-2"onClick={onSubmit}>Submit</BtnPrimary>
+          onSubmit && <BtnPrimary className="m-0 mx-2" onClick={onSubmit}>Submit</BtnPrimary>
         }
       </div>
       <DataList  />
     </>
   )
+}
+
+function InexistentOptionIconWarning() {
+  const { inputText, value } = useContext(Context)!;
+
+  if (inputText !== '' && !value?.value)
+    return (
+      <HiExclamationCircle className="absolute right-2 top-2.5 fill-red-400 w-5 h-5" />
+    )
+  
+  return null;
 }
 
 function DataList() {

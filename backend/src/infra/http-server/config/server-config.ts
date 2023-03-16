ba1d6cdd364a4c1de48464e83cd6db29689bb606
zero <theloostt@live.com>
2023-03-16 00:04:38 -0300

    feat: remove projects functionality

--- backend/src/infra/http-server/config/server-config.ts
+++ backend/src/infra/http-server/config/server-config.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import cors from 'cors';
 import { router as inboxRouter } from '@/controllers/inbox-controller';
 import { router as kaguraRouter } from '@/controllers/kagura-controller';
-import { router as projectsRouter } from '@/controllers/projects-controller';
 
 export const app = express();
 
@@ -11,5 +10,4 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(inboxRouter);
 app.use('/rehearsal', kaguraRouter);
-app.use('/projects', projectsRouter);
 

13b24e3b4fe3bfce763bc907b13482b63c58f24e
zero <theloostt@live.com>
2023-03-21 21:01:29 -0300

    feat: mode button effect

--- frontend/src/features/rehearsal/index.tsx
+++ frontend/src/features/rehearsal/index.tsx
@@ -43,9 +43,66 @@ const data: RehearsalByArea[] = [
             requirements: 'Card 2',
           }
         ]
+      },
+      {
+        category: 'Category 1 A2',
+        cards: [
+          {
+            _id: 'qweqweqweqwe',
+            requirements: 'Card 1',
+          },
+          {
+            _id: 'qweqweqweqwe',
+            requirements: 'lllllllas sadasdasdas asdasdasdasCard 1',
+          },
+          {
+            _id: 'qweqweqweqwe',
+            requirements: 'Questions can come in some many shapes and sizes that I should find a better way to display them.',
+          }
+        ]
+      }
+
+    ]
+  },
+  {
+    area: 'Area 2',
+    practical: [
+      {
+        category: 'Category 1 A2',
+        cards: [
+          {
+            _id: 'qweqweqweqwe',
+            requirements: 'Card 1',
+          },
+          {
+            _id: 'qweqweqweqwe',
+            requirements: 'lllllllas sadasdasdas asdasdasdasCard 1',
+          },
+          {
+            _id: 'qweqweqweqwe',
+            requirements: `Questions can come in some many shapes and sizes that I should find a better way to display them.
+Maybe this will solve my problems, at least I hope so.`,
+          }
+        ]
+      }
+    ],
+    theoretical: [
+      {
+        category: 'Category 1 A2',
+        cards: [
+          {
+            _id: 'qweqweqweqwe',
+            requirements: 'Card 2 question about somthing',
+          },
+          {
+            _id: 'qweqweqweqwe',
+            requirements: 'Card questino of that one thing that its not that other one',
+          }
+        ]
       }
     ]
   }
+
 ]
 
 export function RehearsalConfig() {
@@ -72,20 +129,34 @@ function Mode() {
   const { mode, setMode } = useRehearsalConfigContext()!;
 
   return (
-    <div className='w-full'>
-      <div className='relative w-48'>
-        <div className='relative z-10 w-full flex'>
-          <div className='w-24 h-16' onClick={() => setMode('practical')}>
+    <div className='w-full pt-8 pb-16 flex justify-center items-center'>
+      <div className='relative w-64'>
+        <div className='relative z-10 w-full flex text-tanj-green font-bold text-lg select-none '>
+          <div 
+            className='w-32 h-12 flex justify-center items-center opacity-60 hover:opacity-100' 
+            onClick={() => setMode('practical')}
+            style={{
+              cursor: mode === 'theoretical' ? 'pointer' : undefined,
+              opacity: mode === 'practical' ? '1' : undefined
+            }}
+          >
             <span>Practical</span>
           </div>
-          <div className='w-24 h-16' onClick={() => setMode('theoretical')}>
+          <div 
+            className='w-32 h-12 flex justify-center items-center opacity-60 hover:opacity-100' 
+            onClick={() => setMode('theoretical')}
+            style={{
+              cursor: mode === 'practical' ? 'pointer' : undefined,
+              opacity: mode === 'theoretical' ? '1' : undefined
+            }}
+          >
             <span>Theoretycal</span>
           </div>
         </div>
         <div 
-          className='top-0 absolute left-0 w-24 h-16 bg-tanj-pink rounded-sm'
+          className='top-0 absolute left-0 w-32 h-12 bg-tanj-gray rounded-sm transition'
           style={{
-            left: mode === 'practical' ? '0' : '50%'
+            transform: mode === 'practical' ? 'translateX(0)' : 'translateX(100%)'
           }}
         ></div>
       </div>
@@ -139,8 +210,13 @@ function Deck({deck}:{deck: RehearsalDeck}) {
 }
 
 function Card({card}:{card: RehearsalCard }) {
+  const [isPre, setIsPre] = useState<boolean>(false);
   return (
-    <div className='bg-tanj-white text-black'>
+    <div 
+      className='bg-tanj-white text-black' 
+      style={{whiteSpace: isPre ? 'pre': undefined}} 
+      onClick={() => setIsPre(prev => !prev)} 
+    >
       {card.requirements}
     </div>
   );

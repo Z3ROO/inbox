0e3d9d93b011860f5e4811ca2bc065b59092df6e
zero <theloostt@live.com>
2023-07-02 20:29:23 -0300

    feat: create functional prototype component to add draft as todo

--- frontend/src/features/inbox/api/InsertDraft.ts
+++ frontend/src/features/inbox/api/InsertDraft.ts
@@ -2,14 +2,17 @@ import { queryClient } from "@/App";
 import { API_URL } from "@/config/API";
 import { useMutation, MutationOptions } from "@/lib/query";
 
-type InsertDraftArguments = {content: string}
+type InsertDraftArguments = {
+  content: string
+  todo?: boolean
+}
 
 async function insertDraft(args: InsertDraftArguments) {
-  const { content } = args;
+  const { content, todo } = args;
   
   const request = await fetch(`${API_URL}/inbox`, {
     method: 'post',
-    body: JSON.stringify({content}),
+    body: JSON.stringify({ content, todo }),
     headers: {
       'Content-Type':'application/json'
     }
@@ -24,6 +27,8 @@ export function InsertDraft(options?: MutationOptions<{}, InsertDraftArguments>)
     ...options,
     onSuccess: (data, variables) => {
       queryClient.invalidateQueries('inbox');
+      if (variables.todo)
+        queryClient.invalidateQueries('inbox-todos');
     }
   });
 }
--- frontend/src/features/todo-list/index.tsx
+++ frontend/src/features/todo-list/index.tsx
@@ -1,11 +1,83 @@
 import { List } from "./components/List";
+import { FaPlus } from 'react-icons/fa';
+import { BiCheck } from 'react-icons/bi';
+import { RxCross2 } from 'react-icons/rx';
+import { useState } from 'react';
+import { Textarea } from "@/components/form/Input";
+import * as InboxAPI from '@/features/inbox/api';
 
 export function TodoList() {
+  const [isTodoInputOpen, setIsTodoInputOpen] = useState(false);
+  const [todoInput, setTodoInput] = useState('');
+
+
   return (
     <div className="w-[26rem] h-full p-8 mx-10 flex flex-col rounded-sm">
-      <h4 className="text-tanj-green">Todo list</h4>
+      <div className="flex items-center">
+        <h4 className="p-0 mb-1 inline text-tanj-green mr-4 align-middle">Todo list</h4>
+        <AddTodoButtons {...{isTodoInputOpen, setIsTodoInputOpen ,todoInput, setTodoInput}} />
+      </div>
+      <TodoInput {...{isTodoInputOpen, todoInput, setTodoInput}} />
       <List/>
     </div>
   )
+}
+
+function AddTodoButtons(props: {
+  isTodoInputOpen: boolean, 
+  setIsTodoInputOpen: (value: React.SetStateAction<boolean>) => void
+  todoInput: string, 
+  setTodoInput: (value: React.SetStateAction<string>) => void
+}) {
+  const {isTodoInputOpen, setIsTodoInputOpen, todoInput, setTodoInput} = props;
+  const insertDraft = InboxAPI.InsertDraft();
+
+  return (
+    <>
+      <button
+        className="border border-tanj-green border-opacity-30 opacity-50 hover:opacity-100 p-1 mr-2 rounded-sm transition-all"
+        onClick={() => setIsTodoInputOpen(prev => !prev)}
+      >
+        {
+          isTodoInputOpen ?
+            <RxCross2 className="w-3 h-3 text-tanj-green" /> :
+            <FaPlus className="w-3 h-3 fill-tanj-green" />
+        }
+      </button>
+      {
+        isTodoInputOpen && <button
+          className="border border-tanj-green border-opacity-30 opacity-50 hover:opacity-100 p-1 mr-2 rounded-sm transition-all"
+          onClick={() => {
+            insertDraft({ content: todoInput, todo: true });
+            setIsTodoInputOpen(false);
+            setTodoInput('');
+          }}
+        >
+          <BiCheck className="w-3 h-3 fill-tanj-green" />
+        </button>
+      }
+    </>
+  )
+}
+
+
+function TodoInput(props: {
+  isTodoInputOpen: boolean, 
+  todoInput: string, 
+  setTodoInput: (value: React.SetStateAction<string>) => void
+}) {
+  const {isTodoInputOpen, todoInput, setTodoInput} = props;
+  if (isTodoInputOpen)
+    return (
+      <div className="pt-2 pr-6">
+        <Textarea
+          className="resize-none w-full"
+          value={todoInput}
+          onChange={e => setTodoInput(e.target.value)}
+        />
+      </div>
+    )
+
+  return null
 }
 

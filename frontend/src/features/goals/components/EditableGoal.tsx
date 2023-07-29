a680b46712702741ca794881170a984661f22847
zero <theloostt@live.com>
2023-07-29 19:59:48 -0300

    feat: implement API calls into components

--- frontend/src/features/goals/components/EditableGoal.tsx
+++ frontend/src/features/goals/components/EditableGoal.tsx
@@ -5,24 +5,56 @@ import { Goal } from "./Goal";
 import { Modal } from "@/components/Modal";
 import { useState } from "react";
 import { EditGoal } from "./EditGoal";
+import * as GoalsAPI from '@/features/goals/api';
 
 export function EditableGoal({ goal }: { goal: IGoal }) {
   const [modal, setModal] = useState(false);
+
+  const focusGoal = GoalsAPI.FocusGoal();
+  const activateGoal = GoalsAPI.ActivateGoal();
+
   return (
     <div className="relative">
       <OptionsButton className="absolute top-1 right-1" options={[
         {
           text: goal.focused ? 'Unfocus' : 'Focus',
           disabled: !goal.active,
-          onClick: () => {}
+          onClick: () => {
+            if (goal.focused) {
+              focusGoal({
+                goal_id: goal._id,
+                focus: false
+              });
+            }
+            else {
+              focusGoal({
+                goal_id: goal._id
+              })
+            }
+          }
         },
         {
           text: goal.active ? 'Deactivate' : 'Activate',
-          onClick: () => {}
+          onClick: () => {
+            if (goal.active && goal.focused)
+              return
+
+            if (goal.active) {
+              activateGoal({
+                goal_id: goal._id,
+                activate: false
+              });
+            }
+            else {
+              activateGoal({
+                goal_id: goal._id
+              })
+            }
+          }
         },
         {
           text: 'Edit',
-          onClick: () => { setModal(true)}
+          onClick: () => { setModal(true) }
         },
         {
           text: 'Delete',
@@ -36,7 +68,3 @@ export function EditableGoal({ goal }: { goal: IGoal }) {
     </div>
   )
 }
-
-//<BtnSecondary className='absolute top-1 right-1' onClick={() => setModal(true)} icon>
-//        <AiTwotoneEdit className='text-tanj-green' />
-//      </BtnSecondary>

d61229c99fe46d1f7b0c5162d01b9d9ab3c9cda3
zero <theloostt@live.com>
2023-07-29 00:02:12 -0300

    feat: update into wrapper for editing goals

--- frontend/src/features/goals/components/EditableGoal.tsx
+++ frontend/src/features/goals/components/EditableGoal.tsx
@@ -2,13 +2,20 @@ import { BtnSecondary } from "@/components/Buttons";
 import { AiTwotoneEdit } from "react-icons/ai";
 import { IGoal } from "../types";
 import { Goal } from "./Goal";
+import { Modal } from "@/components/Modal";
+import { useState } from "react";
+import { EditGoal } from "./EditGoal";
 
 export function EditableGoal({ goal }: { goal: IGoal }) {
+  const [modal, setModal] = useState(false);
   return (
     <div className="relative">
-      <BtnSecondary className='absolute top-1 right-1' icon>
+      <BtnSecondary className='absolute top-1 right-1' onClick={() => setModal(true)} icon>
         <AiTwotoneEdit className='text-tanj-green' />
       </BtnSecondary>
+      <Modal isModalOpen={modal} closeFn={() => setModal(false)}>
+        <EditGoal data={goal} onSubmit={() => setModal(false)} />
+      </Modal>
       <Goal {...{goal}} />
     </div>
   )

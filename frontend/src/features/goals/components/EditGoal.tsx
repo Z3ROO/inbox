c20cac187726878ab01fd0c7870c4fd39bc26296
zero <theloostt@live.com>
2023-07-29 00:00:06 -0300

    feat: implement initial Component for edit goals

---
+++ frontend/src/features/goals/components/EditGoal.tsx
@@ -0,0 +1,65 @@
+import { BtnPrimary, BtnSecondary } from "@/components/Buttons";
+import { Input, Textarea } from "@/components/form/Input";
+import { useState } from "react";
+import * as GoalsAPI from '@/features/goals/api';
+import { InputList } from "@/components/form/InputList";
+import { IGoal } from "../types";
+
+export function EditGoal(props: { data: IGoal, onSubmit: () => void}) {
+  const {onSubmit, data} = props;
+  const [title, setTitle] = useState(data.description);
+  const [description, setDescription] = useState(data.description);
+  const [taskList, setTaskList] = useState<{_id: string|undefined, value: string}[]>(data.tasks.map(
+    ({description, _id}) => {
+      return {_id, value: description}
+    }) || []);
+  const [deletedTasks, setDeletedTasks] = useState<{_id: string, value: string}[]>([]);
+
+  const editGoal = GoalsAPI.EditGoal();
+
+  return (
+    <div className="h-[78vh] w-screen max-w-3xl">
+      <h3>Edit Goal</h3>
+      <form className="flex flex-col" id="add-goal" 
+        onSubmit={(e) => {
+          e.preventDefault();
+
+          if (title.trim() === '' || description.trim() === '' || taskList.find(t => t.value.trim() === ''))
+            return;
+
+          editGoal({
+            title,
+            description,
+            tasks: taskList.map( t => ({ _id: t._id, description: t.value })),
+            deletedTasks: deletedTasks.map( t => ({ _id: t._id, description: t.value })),
+          }, {
+            onSuccess(){
+              onSubmit()
+            }
+          });
+        }
+      }>
+        <label>
+          <div>Title: </div>
+          <Input type="text" className="w-full" value={title} onChange={(e) => setTitle(e.target.value)} />
+        </label>
+        <label>
+          <div>Description: </div>
+          <Textarea className="w-full resize-none h-24" value={description} onChange={(e) => setDescription(e.target.value)} />
+        </label>
+        <label>
+          <div>Task list: </div>
+        </label>
+          <InputList values={taskList} setValues={(data) => setTaskList(data)} 
+            onDelete={(data) => {
+              if (data._id !== undefined)
+                setDeletedTasks(prev => {
+                  return prev.concat(data as { _id: string, value: string });
+                })
+            }}
+          />
+      </form>
+      <BtnPrimary form="add-goal">Insert</BtnPrimary>
+    </div>
+  );
+}

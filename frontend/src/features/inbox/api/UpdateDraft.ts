142213e77a0b6ed07eb516b139a5e7ffad98f576
zero <theloostt@live.com>
2023-10-20 19:45:41 -0300

    feat: include jsdoc

--- frontend/src/features/inbox/api/UpdateDraft.ts
+++ frontend/src/features/inbox/api/UpdateDraft.ts
@@ -3,6 +3,17 @@ import { API_URL } from "@/config/API";
 import { useMutation, MutationOptions } from "@/lib/query";
 import { DraftUpdateDTO } from "../types";
 
+/**
+ * #### Updates draft properties based on an specified action.
+ * 
+ * @param args.draft_id - Draft unique identifier.
+ * @param args.content - Draft's text content.
+ * @param args.action - Specific action to be taken in the server-side.
+ * @param quantity - Used along with delay type actions, specifies a quantity of delay
+ * @param priority - Updated draft's priority.
+ * @param category - Updated draft's category.
+ * @returns - !!! TO BE STANDARDIZED !!!
+ */
 
 async function updateDraft(args: DraftUpdateDTO): Promise<{}> {
   const { content, draft_id, action, quantity, priority, category } = args;
@@ -24,7 +35,6 @@ export function UpdateDraft(options?: MutationOptions<{}, DraftUpdateDTO>) {
   return useMutation(updateDraft, {
     ...options,
     onSuccess: (_, variables) => {
-
       queryClient.invalidateQueries('inbox-todos');
       queryClient.invalidateQueries('inbox');
     }

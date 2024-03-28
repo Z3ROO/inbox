181718e7ab4d0183b45d20caf878c506a19f80cd
zero <theloostt@live.com>
2024-03-28 10:55:16 -0300

    feat: documentation page

---
+++ frontend/src/pages/UIDoc/components.ts
@@ -0,0 +1,11 @@
+export interface ComponentMetaData {
+  name: string
+  directory: string
+}
+
+export const components = [
+  {
+    name: 'Dropdown Menu',
+    directory: '../../components/dropdown/DropDownMenu.sample.tsx'
+  }
+]

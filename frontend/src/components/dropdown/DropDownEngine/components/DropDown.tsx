6b8f251c117b6e92d4913a20e6dfa53e32e4e453
zero <theloostt@live.com>
2024-03-28 10:55:47 -0300

    fixes and sample page creation for dropdownmenu

--- frontend/src/components/dropdown/DropDownEngine/components/DropDown.tsx
+++ frontend/src/components/dropdown/DropDownEngine/components/DropDown.tsx
@@ -1,13 +1,13 @@
-import { ReactNode } from "react"
+import { ReactNode, useMemo } from "react"
 import { DropDownContextProvider } from "../store/Context";
 
 
 export function DropDownEngine({ children }: { children?: ReactNode }) {
-
+  const key = useMemo(() => Date.now(), []);
   return (
     <DropDownContextProvider>
       <div className="inline-block">
-        <div className="relative z-10" onClick={e => e.stopPropagation()} onMouseMove={e => e.stopPropagation()}>
+        <div className="relative z-10" id={key.toString()} onClick={e => e.stopPropagation()} onMouseMove={e => e.stopPropagation()}>
           {children}
         </div>
       </div>
--- frontend/src/components/dropdown/DropDownMenu.sample.tsx
+++ frontend/src/components/dropdown/DropDownMenu.sample.tsx
@@ -1,16 +1,27 @@
-import { DropDownMenu, DropDownMenuTriggerOnClick, DropDownMenuContent, DropDownMenuItem } from "./DropDownMenu"
+import { DropDownMenu, DropDownMenuTriggerOnClick, DropDownMenuContent, DropDownMenuItem, DropDownMenuTriggerOnHover } from "./DropDownMenu"
 
 export function ComponentSample() {
   return (
-    <div className="inline">
-      <DropDownMenu>
-        <DropDownMenuTriggerOnClick>Open</DropDownMenuTriggerOnClick>
-        <DropDownMenuContent>
-          <DropDownMenuItem>Opção 1</DropDownMenuItem>
-          <DropDownMenuItem>Opção 2</DropDownMenuItem>
-          <DropDownMenuItem>Opção 3</DropDownMenuItem>
-        </DropDownMenuContent>
-      </DropDownMenu>
+    <div>
+      
+        <DropDownMenu>
+          <DropDownMenuTriggerOnClick>Click</DropDownMenuTriggerOnClick>
+          <DropDownMenuContent>
+            <DropDownMenuItem>Opção 1</DropDownMenuItem>
+            <DropDownMenuItem>Opção 2</DropDownMenuItem>
+            <DropDownMenuItem>Opção 3</DropDownMenuItem>
+          </DropDownMenuContent>
+        </DropDownMenu>
+      
+        <DropDownMenu>
+          <DropDownMenuTriggerOnHover>Hover</DropDownMenuTriggerOnHover>
+          <DropDownMenuContent>
+            <DropDownMenuItem>Opção 1</DropDownMenuItem>
+            <DropDownMenuItem>Opção 2</DropDownMenuItem>
+            <DropDownMenuItem>Opção 3</DropDownMenuItem>
+          </DropDownMenuContent>
+        </DropDownMenu>
+      
     </div>
   )
 }

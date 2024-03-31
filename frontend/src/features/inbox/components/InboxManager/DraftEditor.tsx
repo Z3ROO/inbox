a668649f8b305bd67f4305be7b28e727f60f6edd
zero <theloostt@live.com>
2024-03-31 01:06:16 -0300

    feat: isolate draftItems component offering an end of journey callback function

--- frontend/src/features/inbox/components/InboxManager/DraftEditor.tsx
+++ frontend/src/features/inbox/components/InboxManager/DraftEditor.tsx
@@ -3,9 +3,8 @@ import { useEffect, useState } from 'react';
 import { useInboxContext } from '../../store/InboxContext';
 import * as InboxAPI from '@/features/inbox/api';
 import { LoadingSpinner } from '@/components/Loading';
-import { IDraft, DraftItemDTO } from 'shared-types';
-import { Modal } from '@/components/Modal';
 import { DropDownMenu, DropDownMenuContent, DropDownMenuItem, DropDownMenuTriggerOnClick } from '@/components/dropdown';
+import { NewDraftItem } from './NewDraftItem';
 
 export function DraftEditor(props: React.HTMLAttributes<HTMLDivElement>){
   const { inboxManagerTextarea, setInboxManagerTextarea, inboxManagerTitle, setInboxManagerTitle } = useInboxContext()!;
@@ -35,11 +34,11 @@ export function DraftEditor(props: React.HTMLAttributes<HTMLDivElement>){
   )
 }
 
-function DraftItems() {
+function DraftItems(props: {}) {
   const { data: inbox } = InboxAPI.QueryInbox();
   const currentDraft = inbox![0];
-  const [newItem, setNewItem] = useState<DraftItemDTO|null>(null);
   const {data: items} = InboxAPI.QueryDraftItems({draft_id: currentDraft._id});
+  const attachDraftItem = InboxAPI.AttachDraftItem();
   const detachDraftItem = InboxAPI.DetachDraftItem();
 
   return (
@@ -60,84 +59,7 @@ function DraftItems() {
           </div>
         ))
       }
-      {
-        newItem ? (
-          <ChooseNewDraftItem {...{newItem, setNewItem}} />
-        ) : (
-          <ChooseItemTypeButton  {...{newItem, setNewItem}} />
-        )
-      }
-    </>
-  )
-}
-
-function ChooseNewDraftItem({newItem, setNewItem}: { newItem: DraftItemDTO, setNewItem: React.Dispatch<React.SetStateAction<DraftItemDTO | null>> }) {
-  const attachDraftItem = InboxAPI.AttachDraftItem();
-  const { data: inbox } = InboxAPI.QueryInbox();
-  const currentDraft = inbox![0];
-
-  return (
-    <div className='flex'>
-      <ChooseItemTypeButton  {...{newItem, setNewItem}} />
-      {
-        newItem.type === 'new' && (
-          <div>
-            <input value={newItem.value} onChange={e => setNewItem({type: 'new', value: e.target.value})} />
-            <button className='px-1.5 rounded border' onClick={() => {setNewItem(null)}}>XX</button>
-          </div>
-        )
-      }
-      {
-        newItem.type === 'existing' && (
-          <SearchDrafts result={(draft) => { setNewItem({type:'existing', value: draft._id})}} />
-        )
-      }
-      <button className='px-1.5 rounded border' onClick={() => { attachDraftItem({draft_id: currentDraft._id, ...newItem}); setNewItem(null) }} disabled={newItem.value.trim() === ''}>send</button>
-    </div>
-  )
-}
-
-function ChooseItemTypeButton({newItem, setNewItem}: { newItem: DraftItemDTO|null, setNewItem: React.Dispatch<React.SetStateAction<DraftItemDTO | null>> }) {
-  return (
-    <div className='w-min relative'>
-      <DropDownMenu>
-        <DropDownMenuTriggerOnClick>+</DropDownMenuTriggerOnClick>
-        <DropDownMenuContent position='top' align='start'>
-          <DropDownMenuItem onClick={() => setNewItem({type: 'new', value: ''})}>New</DropDownMenuItem>
-          <DropDownMenuItem onClick={() => setNewItem({type: 'existing', value: ''})}>Existing</DropDownMenuItem>
-        </DropDownMenuContent>
-      </DropDownMenu>
-    </div>
-  )
-}
-
-function SearchDrafts(props: {result:(draft:IDraft) => void}) {
-  const [modal, setModal] = useState(false);
-  const [searchInput, setSearchInput] = useState('');
-  const [search, setSearch] = useState('');
-  const { data: searchResults } = InboxAPI.SearchDrafts({search});
-
-  return (
-    <>
-      <div>
-        <button onClick={() => setModal(true)}>Search drafts</button>
-      </div>
-      <Modal isModalOpen={modal} closeFn={() => setModal(false)}>
-        <input value={searchInput} onChange={e => setSearchInput(e.target.value)}/>
-        <div className='overflow-auto max-h-[36rem]'>
-        <button onClick={() => setSearch(searchInput)}>search</button>
-        <div className='max-w-3xl flex flex-wrap'>
-          {
-            (searchResults || []).map(draft => (
-              <div className='p-4 m-2 rounded-sm border text-white border-white w-80' onClick={()=> {props.result(draft); setModal(false)}}>
-                {draft.title && <h4>{draft.title}</h4>}
-                <p>{draft.content}</p>
-              </div>
-            ))
-          }
-        </div>
-        </div>
-      </Modal>
+      <NewDraftItem cb={(newItem) => { attachDraftItem({draft_id: currentDraft._id, ...newItem}) }} />
     </>
   )
 }
---
+++ frontend/src/features/inbox/components/InboxManager/NewDraftItem.tsx
@@ -0,0 +1,89 @@
+import { Modal } from "@/components/Modal";
+import { DropDownMenu, DropDownMenuTriggerOnClick, DropDownMenuContent, DropDownMenuItem } from "@/components/dropdown";
+import { Checkbox } from "@/components/icons/UI";
+import { useState } from "react";
+import { DraftItemDTO, IDraft } from "shared-types";
+import * as InboxAPI from '@/features/inbox/api';
+
+export function NewDraftItem({cb}: { cb: ((newItem: DraftItemDTO) => void)}) {
+  const [newItem, setNewItem] = useState<DraftItemDTO|null>(null);
+
+  if (newItem)
+    return (
+      <ChooseNewDraftItem {...{newItem, setNewItem, cb}} />
+    )
+  
+  return (
+    <ChooseItemTypeButton  {...{newItem, setNewItem}} />
+  )
+}
+
+function ChooseNewDraftItem({newItem, setNewItem, cb}: { cb: ((newItem: DraftItemDTO) => void), newItem: DraftItemDTO, setNewItem: React.Dispatch<React.SetStateAction<DraftItemDTO | null>> }) {
+  
+  return (
+    <div className='flex'>
+      <ChooseItemTypeButton  {...{newItem, setNewItem}} />
+      {
+        newItem.type === 'new' && (
+          <div>
+            <input value={newItem.value} onChange={e => setNewItem({type: 'new', value: e.target.value})} />
+            <button className='px-1.5 rounded border' onClick={() => {setNewItem(null)}}>XX</button>
+          </div>
+        )
+      }
+      {
+        newItem.type === 'existing' && (
+          <SearchDrafts result={(draft) => { setNewItem({type:'existing', value: draft._id})}} />
+        )
+      }
+      <button className='px-1.5 rounded border' onClick={() => { cb(newItem); setNewItem(null) }} disabled={newItem.value.trim() === ''}>send</button>
+    </div>
+  )
+}
+
+function ChooseItemTypeButton({newItem, setNewItem}: { newItem: DraftItemDTO|null, setNewItem: React.Dispatch<React.SetStateAction<DraftItemDTO | null>> }) {
+  return (
+    <div className='w-min relative'>
+      <DropDownMenu>
+        <DropDownMenuTriggerOnClick>
+          <Checkbox.doted className='w-6' />
+        </DropDownMenuTriggerOnClick>
+        <DropDownMenuContent position='top' align='start'>
+          <DropDownMenuItem onClick={() => setNewItem({type: 'new', value: ''})}>New</DropDownMenuItem>
+          <DropDownMenuItem onClick={() => setNewItem({type: 'existing', value: ''})}>Existing</DropDownMenuItem>
+        </DropDownMenuContent>
+      </DropDownMenu>
+    </div>
+  )
+}
+
+function SearchDrafts(props: {result:(draft:IDraft) => void}) {
+  const [modal, setModal] = useState(false);
+  const [searchInput, setSearchInput] = useState('');
+  const [search, setSearch] = useState('');
+  const { data: searchResults } = InboxAPI.SearchDrafts({search});
+
+  return (
+    <>
+      <div>
+        <button onClick={() => setModal(true)}>Search drafts</button>
+      </div>
+      <Modal isModalOpen={modal} closeFn={() => setModal(false)}>
+        <input value={searchInput} onChange={e => setSearchInput(e.target.value)}/>
+        <div className='overflow-auto max-h-[36rem]'>
+        <button onClick={() => setSearch(searchInput)}>search</button>
+        <div className='max-w-3xl flex flex-wrap'>
+          {
+            (searchResults || []).map(draft => (
+              <div className='p-4 m-2 rounded-sm border text-white border-white w-80' onClick={()=> {props.result(draft); setModal(false)}}>
+                {draft.title && <h4>{draft.title}</h4>}
+                <p>{draft.content}</p>
+              </div>
+            ))
+          }
+        </div>
+        </div>
+      </Modal>
+    </>
+  )
+}

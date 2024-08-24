cb48d5f1e937b175add709b3f7079587ee8bff5b
zero <theloostt@live.com>
2024-08-24 20:57:24 -0300

    chore: todo comment

--- frontend/src/config/API.ts
+++ frontend/src/config/API.ts
@@ -1,4 +1,6 @@
 const PROD = import.meta.env.PROD;
 
+//TODO: Use env variables instead of this hardcode.
 const API_PORT = PROD ? '1338' : '3001';
+
 export const API_URL = `http://localhost${API_PORT && (':'+API_PORT)}`;

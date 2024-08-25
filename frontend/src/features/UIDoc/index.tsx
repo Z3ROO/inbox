2fee4932a39f3c83ff3dc6ae95b946866b8b9d1c
zero <theloostt@live.com>
2024-08-24 21:06:14 -0300

    chore: Todo comment

--- frontend/src/features/UIDoc/index.tsx
+++ frontend/src/features/UIDoc/index.tsx
@@ -1,3 +1,4 @@
 export * from './components/ComponentSample'
 export * from './components/ComponentsListSidebar'
 export * from './components/UIDocumentationPage'
+//TODO: AI Doc Page should be within NanoUI package
